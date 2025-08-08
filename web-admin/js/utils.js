a3f8bf916e951b20af5180de777fe030b938b284
Luis Aguilar <laguilar_systems@hotmail.com>
2025-08-08 11:11:38 -0500

    Login

--- web-admin/js/utils.js
+++ web-admin/js/utils.js
@@ -10,13 +10,7 @@ export const apiRequest = async (endpoint, method = "GET", body = null, headers 
         if (body) {
             options.body = JSON.stringify(body);
         }
-
         const response = await fetch(endpoint, options);
-
-        if (!response.ok) {
-            throw new Error(`Error ${response.status}: ${response.statusText}`);
-        }
-
         return await response.json();
     } catch (error) {
         console.error("Error en la petición:", error);
